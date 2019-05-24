6d8da93156883de8d3624cd88d20dfc7d01aeec6
manother <why685203@163.com>
2019-05-24 10:03:04 +0800

    ceshidemo
    ceshidemo

---
+++ files/应用管理/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj)),S,_(),bk,_(),bl,_(bm,n,b,bn,bo,be))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="应用管理.html",d="generationDate",e=new Date(1558663344571.8),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="325ff07e35894a66bee23c30d6f27f4e",q="type",r="Axure:Page",s="name",t="应用管理",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="130c49586f9b4fef8de7116646dcc4f6",Y="label",Z="friendlyType",ba="内联框架",bb="inlineFrame",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=1726,bi="height",bj=814,bk="imageOverrides",bl="target",bm="targetType",bn="应用列表.html",bo="includeVariables",bp="masters",bq="objectPaths",br="130c49586f9b4fef8de7116646dcc4f6",bs="scriptId",bt="u19050";
+return _creator();
+})());
